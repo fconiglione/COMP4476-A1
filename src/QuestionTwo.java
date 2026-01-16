994034b9f17bbc8be8098a1bae3537753e1f87a3
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2026-01-16 15:24:38 -0500

    completed q2

--- src/QuestionTwo.java
+++ src/QuestionTwo.java
@@ -35,19 +35,19 @@ public class QuestionTwo {
     }
 
     public static String encrypt(String text, int[] key, int m) {
-        StringBuilder paddedText = new StringBuilder(text);
-        // Add padding to ensure text fits within the permutation size (m)
-        while (paddedText.length() % m != 0) {
-            paddedText.append(" ");
+        StringBuilder fittedText = new StringBuilder(text);
+        // Add extra space to ensure text fits within the permutation size (m)
+        while (fittedText.length() % m != 0) {
+            fittedText.append(" ");
         }
 
         StringBuilder result = new StringBuilder();
 
         // Develop the new encrypted block
-        for (int i = 0; i < paddedText.length(); i += m) {
+        for (int i = 0; i < fittedText.length(); i += m) {
             char[] block = new char[m];
             for (int j = 0; j < m; j++) {
-                block[key[j]] = paddedText.charAt(i + j);
+                block[key[j]] = fittedText.charAt(i + j);
             }
             result.append(new String(block));
         }
@@ -55,10 +55,10 @@ public class QuestionTwo {
     }
 
     public static String decrypt(String text, int[] key, int m) {
-        // Create the inverse key for decryption
-        int[] inverseKey = new int[m];
+        // Create the decryption key for decryption
+        int[] decryptionKey = new int[m];
         for (int i = 0; i < m; i++) {
-            inverseKey[key[i]] = i;
+            decryptionKey[key[i]] = i;
         }
 
         StringBuilder result = new StringBuilder();
@@ -67,7 +67,7 @@ public class QuestionTwo {
         for (int i = 0; i < text.length(); i += m) {
             char[] block = new char[m];
             for (int j = 0; j < m; j++) {
-                block[inverseKey[j]] = text.charAt(i + j);
+                block[decryptionKey[j]] = text.charAt(i + j);
             }
             result.append(new String(block));
         }
